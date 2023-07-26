04a3d4e68dc625e50531114e38f65b980710142a
Rankshow <uwaomaobinna20@gmail.com>
2023-07-26 14:32:50 +0100

    removed home list

--- src/Dashboard/Navbar.jsx
+++ src/Dashboard/Navbar.jsx
@@ -10,7 +10,6 @@ const Navbar = ({ setIsAuthenticated, setIsAdding }) => {
         <ul className='flex items-center pl-4 justify-between bg-black '>
          <h1 className='md:text-3xl text-red-600 font-bold md:py-5 py-5'>Leave Management System</h1>
           <div className='flex cursor-pointer text-white md:mr-4 mr-2'>
-          <li className='mr-3 font-medium hover:opacity-[.7]'>Home</li>
            <li className=' font-medium hover:opacity-[.7]'><Logout setIsAuthenticated={setIsAuthenticated} /></li>
             </div> 
         </ul>
