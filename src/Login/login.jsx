571cee954878c0dde6f0b3cc4e47e00bcaca9f9b
rankshow <uwaomaobinna20@gmail.com>
2023-07-29 02:51:06 +0100

    change the login Ui

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,58 +1,97 @@
 import React, { useState } from "react";
 import Swal from "sweetalert2";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 
 const Login = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const handleLogin = (e) => {
+  const handleLogin =  (e) => {
     e.preventDefault();
 
     //Authenticating a user
-    try {
-      signInWithEmailAndPassword(auth, email, password);
-      Swal.fire({
-        timer: 1500,
-        showConfirmButton: false,
-        willOpen: () => {
-          Swal.showLoading();
-        },
-        willClose: () => {
-          setIsAuthenticated(true);
-          Swal.fire({
-            icon: "success",
-            title: "Successfully logged in!",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-        },
-      });
-    } catch(error) {
-      Swal.fire({
-        timer: 1500,
-        showConfirmButton: false,
-        willOpen: () => {
-          Swal.showLoading();
-        },
-        willClose: () => {
-          Swal.fire({
-            icon: "error",
-            title: "Error!",
-            text: "Incorrect email or password.",
-            showConfirmButton: true,
-          });
-        },
-      });
-    }
+    if(document.activeElement.name  === "Login"){
+      try {
+        signInWithEmailAndPassword(auth, email, password);
+        Swal.fire({
+          timer: 1500,
+          showConfirmButton: false,
+          willOpen: () => {
+            Swal.showLoading();
+          },
+          willClose: () => {
+            setIsAuthenticated(true);
+            Swal.fire({
+              icon: "success",
+              title: "Successfully logged in!",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          },
+        });
+      } catch(error) {
+        Swal.fire({
+          timer: 1500,
+          showConfirmButton: false,
+          willOpen: () => {
+            Swal.showLoading();
+          },
+          willClose: () => {
+            Swal.fire({
+              icon: "error",
+              title: "Error!",
+              text: "Incorrect email or password.",
+              showConfirmButton: true,
+            });
+          },
+        });
+      }
+    } else if(document.activeElement.name === "Register") { 
+      try {
+        createUserWithEmailAndPassword(auth, email, password)
+        Swal.fire({
+          timer: 1500,
+          showConfirmButton: false,
+          willOpen: () => {
+            Swal.showLoading();
+          },
+          willClose: () => {
+            setIsAuthenticated(true);
+            Swal.fire({
+              icon: "success",
+              title: "Successfully register and logged in!",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          },
+        });
+      } catch (error) {
+        console.log(error)
+        Swal.fire({
+          timer: 1500,
+          showConfirmButton: false,
+          willOpen: () => {
+            Swal.showLoading();
+          },
+          willClose: () => {
+            Swal.fire({
+              icon: "error",
+              title: "Error!",
+              text: "Incorrect email or password.",
+              showConfirmButton: true,
+            });
+          },
+        });
+      }
+    }   
   };
 
   // rendering to the Dom
   return (
-    <div className="bg-gradient-to-b from-indigo-800  max-w-3xl h-screen flex items-center justify-center mx-auto">
-      <form className="flex justify-center flex-col items-center ">
-        <h1 className="text-2xl md:text-2xl font-medium text-red-800 mb-3">
+    <div className=" max-w-3xl h-screen flex items-center justify-center mx-auto">
+      <form className="w-[50vw] shadow-slate-800 rounded-se-md flex justify-center flex-col items-center h-[500px] bg-black">
+        <h1 className="text-2xl md:text-2xl font-medium text-white mb-3">
           Login to Leave management Account
         </h1>
         {/* Email */}
@@ -76,12 +115,22 @@ const Login = ({ setIsAuthenticated }) => {
           placeholder="enter your password"
         />
         {/*Login*/}
+        <div>
         <button
           onClick={handleLogin}
-          className="bg-blue-800 py-2 px-2 mt-3 font-bold text-white rounded-md hover:opacity-[.7]"
+          className="bg-blue-800 tracking-wide py-2 px-2 mt-3 mr-6 font-bold text-white rounded-md hover:opacity-[.7]"
+          name="Login"
         >
           Login
         </button>
+        <button
+          onClick={handleLogin}
+          className="bg-green-500 py-2 px-2 mt-3 font-bold text-white rounded-md hover:opacity-[.7]"
+          name="Register"
+        >
+          Register
+        </button>
+        </div>
       </form>
     </div>
   );
--- src/logout/logout.jsx
+++ src/logout/logout.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import Swal from 'sweetalert2';
+import { signOut } from "firebase/auth";
+import { auth } from '../config/firebase';
 
 const Logout = ({ setIsAuthenticated }) => {
+
   const handleLogout = () => {
-    Swal.fire({
-      icon: 'question',
-      title: 'Logging Out',
-      text: 'Are you sure you want to log out?',
-      showCancelButton: true,
-      confirmButtonText: 'Yes',
-    }).then(result => {
-      if (result.value) {
-        Swal.fire({
-          timer: 1500,
-          showConfirmButton: false,
-          willOpen: () => {
-            Swal.showLoading();
-          },
-          willClose: () => {
-            localStorage.setItem('is_authenticated', false);
-            setIsAuthenticated(false);
-          },
-        });
-      }
+    signOut(auth).then(() => {
+      Swal.fire({
+        icon: 'question',
+        title: 'Logging Out',
+        text: 'Are you sure you want to log out?',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+      }).then(result => {
+        if (result.value) {
+          Swal.fire({
+            timer: 1500,
+            showConfirmButton: false,
+            willOpen: () => {
+              Swal.showLoading();
+            },
+            willClose: () => {
+              setIsAuthenticated(false);
+            },
+          });
+        }
+      });
+    }).catch((error) => {
+      console.log(error)
     });
-  };
+};
 
   return (
     <h1 onClick={handleLogout}>Logout</h1>
